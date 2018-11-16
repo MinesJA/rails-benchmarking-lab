e71adf1ab73de2bc9f59db9216a6000f2b519582
Jonathan Mines <minesja@gmail.com>
2018-11-16 17:52:01 -0500

    Tested abstraction of parsing into lib

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,35 +1,7 @@
-require 'csv'
-
 class Actor < ApplicationRecord
   has_many :movie_actors
   has_many :movies, through: :movie_actors
   validates :name, uniqueness: true
-
-  # def self.seed_actors
-  #   CSV.foreach("db/the-movies-dataset/tmdb_5000_credits.csv") do |row|
-  #     # ["movie_id", "title", "cast", "crew"]
-  #     movie_id, title, cast, crew = row
-  #
-  #     if(movie_id != "movie_id")
-  #       @movie = Movie.find(movie_id.to_i)
-  #
-  #       if(cast[0] == "[" && cast[-1] == "]")
-  #         cast_array = eval(cast)
-  #         cast_array.each do |cast_obj|
-  #           # cast_obj ex. {:cast_id=>242, :character=>"Jake Sully", :credit_id=>"5602a8a7c3a3685532001c9a", :gender=>2, :id=>65731, :name=>"Sam Worthington", :order=>0}
-  #
-  #           @actor = Actor.find_or_create_by(name: cast_obj[:name])
-  #           if !@actor.gender
-  #             @actor.update(gender: cast_obj[:gender])
-  #           end
-  #           @movie_actor = MovieActor.find_or_create_by(movie_id: @movie.id, actor_id: @actor.id, character: cast_obj[:character])
-  #           puts "Created/found #{@actor.name}"
-  #
-  #         end
-  #       end
-  #     end
-  #   end
-  # end
 
 
 
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,49 +1,9 @@
-require 'csv'
-
 class Movie < ApplicationRecord
   has_many :movie_actors
   has_many :actors, through: :movie_actors
 
   has_many :keyword_movies
   has_many :keywords, through: :keyword_movies
-
-  # def self.seed_movies
-  #   CSV.foreach("db/the-movies-dataset/tmdb_5000_movies.csv") do |row|
-  #     # ["budget", "genres", "homepage", "id", "keywords", "original_language", "original_title", "overview", "popularity", "production_companies", "production_countries", "release_date", "revenue", "runtime", "spoken_languages", "status", "tagline", "title", "vote_average", "vote_count"]
-  #     budget, genres, homepage, id, keywords, original_language, original_title, overview, popularity, production_companies, production_countries, release_date, revenue, runtime, spoken_languages, status, tagline, title, vote_average, vote_count = row
-  #
-  #     if(budget != "budget")
-  #       @movie = Movie.new(
-  #         title: original_title,
-  #         overview: overview,
-  #         tagline: tagline,
-  #         runtime: runtime,
-  #         popularity: popularity.to_f,
-  #         original_language: original_language,
-  #         budget: budget.to_i,
-  #         revenue: revenue.to_i,
-  #         vote_count: vote_count,
-  #         vote_average: vote_average.to_f,
-  #         status: status
-  #       )
-  #
-  #       @movie.id = id
-  #       @movie.save
-  #
-  #       puts "Created #{@movie.title}"
-  #
-  #       Keyword.seed_keywords(keywords, @movie)
-  #     end
-  #   end
-  # end
-
-
-
-
-
-
-
-
 
 
 
