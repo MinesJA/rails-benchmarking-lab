07baa48f1866f5c87901f09c50473d94f71c846e
Jonathan Mines <minesja@gmail.com>
2018-11-17 10:29:09 -0500

    added example of how to benchmark queries in the query controller.

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -2,8 +2,25 @@ class QueryController < ApplicationController
 
 
   def index
+    # tests should be return of Rspec tests
+    @tests = [
+      {passed: false,
+       deliverable: "First Return all movies with the name of blank in this order",
+     },
+      {passed: true,
+       deliverable: "Second Return all movies with the name of blank in this order"},
+      {passed: false,
+       deliverable: "Third Return all movies with the name of blank in this order"},
+      {passed: true,
+       deliverable: "Fourth Return all movies with the name of blank in this order"},
+      {passed: true,
+       deliverable: "Fifth Return all movies with the name of blank in this order"}
+      ]
     # @test_one = Benchmark.run(Movie.method{deliverable: "Return all movies with the name of something in the order of something else something", passed: true}
-    
+
+    # CPU time, system CPU time, the sum of the user and system CPU times, and the elapsed real time. The unit of time is seconds.
+    @benchmark_bad = Benchmark.measure { Movie.find_by(title: "Avatar") }
+    @benchmark_good = Benchmark.measure { Movie.all.find {|movie| movie.title == "Avatar"} }
 
     # @benchmark_one = Benchmark.run(Movie.theirmethod)
     # @query_one = {query: "", speed: 0.0324}
